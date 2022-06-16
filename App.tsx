6f791eae19df450a55cf81ada69a22a96ed1d4d1
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-06-16 15:29:12 -0300

    styles: add theme and font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { theme } from './src/theme';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_900Black,
+} from '@expo-google-fonts/montserrat';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text_primary_regular}>Regular primary size 20</Text>
+      <Text style={styles.text_secondary_medium}>Medium secondary size 18</Text>
+      <Text style={styles.text_on_brand_semi_bold}>Semi bold on brand size 16</Text>
+      <Text style={styles.text_primary_black_border}>Black primary with border size 14</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text_primary_regular:{
+    color: theme.colors.text_primary,
+    fontFamily: theme.fonts.regular,
+    marginBottom: 12,
+    fontSize: 20,
+  },
+  text_secondary_medium:{
+    color: theme.colors.text_secondary,
+    fontFamily: theme.fonts.medium,
+    marginBottom: 12,
+    fontSize: 18,
+  },
+  text_on_brand_semi_bold:{
+    color: theme.colors.text_on_brand,
+    fontFamily: theme.fonts.semi_bold,
+    backgroundColor: theme.colors.brand,
+    padding: 5,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  text_primary_black_border: {
+    color: theme.colors.text_primary,
+    fontFamily: theme.fonts.black,
+    padding: 5,
+    borderWidth: 1,
+    fontSize: 14,
+    borderColor: theme.colors.border,
+  }
+
 });
---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    background: '#F5F5F5',
+    brand: '#39C5C7',
+
+    surface_primary: 'transparent',
+    surface_secondary: '#39C5C7',
+
+    text_primary: '#697581',
+    text_secondary: '#39C5C7',
+    text_on_brand: '#E0E0E0',
+
+    border: '#39C5C7',
+  },
+
+  fonts: {
+    regular: 'Montserrat_400Regular',
+    medium: 'Montserrat_500Medium',
+    semi_bold: 'Montserrat_600SemiBold',
+    black: 'Montserrat_900Black',
+  }
+};
+
+
