97c9a35bd1be845143dcc8c7b4bf2dbef2db1464
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-06-16 16:03:18 -0300

    style: prettier

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,24 +1,22 @@
 export const theme = {
   colors: {
-    background: '#F5F5F5',
-    brand: '#39C5C7',
+    background: "#F5F5F5",
+    brand: "#39C5C7",
 
-    surface_primary: 'transparent',
-    surface_secondary: '#39C5C7',
+    surface_primary: "transparent",
+    surface_secondary: "#39C5C7",
 
-    text_primary: '#697581',
-    text_secondary: '#39C5C7',
-    text_on_brand: '#E0E0E0',
+    text_primary: "#697581",
+    text_secondary: "#39C5C7",
+    text_on_brand: "#E0E0E0",
 
-    border: '#39C5C7',
+    border: "#39C5C7",
   },
 
   fonts: {
-    regular: 'Montserrat_400Regular',
-    medium: 'Montserrat_500Medium',
-    semi_bold: 'Montserrat_600SemiBold',
-    black: 'Montserrat_900Black',
-  }
+    regular: "Montserrat_400Regular",
+    medium: "Montserrat_500Medium",
+    semi_bold: "Montserrat_600SemiBold",
+    black: "Montserrat_900Black",
+  },
 };
-
-
